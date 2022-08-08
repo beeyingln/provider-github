d30816cbeb9f76c11ae6627aef3ae18d9dca56ea
vagrant <bee_ying.lo@nokia.com>
2022-08-08 14:03:50 +0000

    Rename file

--- run-github-provider.sh
+++ run-github-provider.sh
@@ -6,4 +6,5 @@ export TERRAFORM_PROVIDER_DOWNLOAD_NAME="terraform-provider-github"
 export TERRAFORM_PROVIDER_DOWNLOAD_URL_PREFIX="https://releases.hashicorp.com/terraform-provider-github/4.19.2"
 export TERRAFORM_NATIVE_PROVIDER_BINARY="terraform-provider-github_4.19.2"
 
+make submodules
 make run
